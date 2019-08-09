96f58d0092c9509a597d5022f3dd513afa6b9178
Gabriel Pan Gantes <gabrielpanga@gmail.com>
2019-08-09 16:51:34 +0200

    fix: eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,18 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/airbnb',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-underscore-dangle': 'off',
+    'vue/no-v-html': 'off',
+    'import/extensions': ['error', 'always', {
+      js: 'never',
+      vue: 'never',
+    }],
   },
   parserOptions: {
     parser: 'babel-eslint',
