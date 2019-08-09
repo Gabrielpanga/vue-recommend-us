d55791a760ffb89d7a0ffeed2ae9d596d0a54e36
Gabriel Pan Gantes <gabrielpanga@gmail.com>
2019-08-09 16:48:57 +0200

    feat: added setup for firebase

--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
 import Vue from 'vue';
-import App from './App.vue';
+import App from './App';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import '@mdi/font/css/materialdesignicons.css'
+import setupFirebase from './plugins/firebase';
+
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import '@mdi/font/css/materialdesignicons.css';
 
 Vue.config.productionTip = false;
+
+setupFirebase();
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app');
